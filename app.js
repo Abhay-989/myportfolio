f69e2b6c889fe27f5df4ac8fd8d6bc64b5823807
Abhay-989 <abhaychouhan8799@gmail.com>
2025-04-19 09:21:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const express= require("express");
+const app = express();
+app.set("view engine","ejs");
+const path=require("path");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.static('public')); 
+app.get("/home",(req,res)=>{
+    res.render("Home.ejs")
+})
+app.get("/about",(req,res)=>{
+    res.render("About.ejs")
+})
+app.get("/skills",(req,res)=>{
+    res.render("Skills.ejs")
+})
+app.get("/project",(req,res)=>{
+    res.render("Project.ejs")
+})
+app.get("/contact",(req,res)=>{
+    res.render("Contact.ejs")
+})
+
+
+app.listen(8080,()=>{
+    console.log(`app listening on port 8080`)
+})
