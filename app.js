ff7efa37f7e1d14c529f8cd7134ce5459cc15e2e
Abhay-989 <abhaychouhan8799@gmail.com>
2025-05-30 18:58:45 +0530

    commited

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const express = require("express");
+
+const express= require("express");
 const app = express();
-const path = require("path");
+app.set("view engine","ejs");
+const path=require("path");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.static('public')); 
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "src", "views"));
-app.use(express.static("public"));
+app.get("/",(req,res)=>{
+    res.render("home.ejs")
+})
+app.get("/about",(req,res)=>{
+    res.render("about.ejs")
+})
+app.get("/skills",(req,res)=>{
+    res.render("skills.ejs")
+})
+app.get("/project",(req,res)=>{
+    res.render("project.ejs")
+})
+app.get("/contact",(req,res)=>{
+    res.render("contact.ejs")
+})
 
-app.get("/", (req, res) => {
-  res.render("home.ejs");
-});
-app.get("/about", (req, res) => {
-  res.render("about.ejs");
-});
-app.get("/home", (req, res) => {
-  res.render("home.ejs");
-});
-app.get("/skills", (req, res) => {
-  res.render("skills.ejs");
-});
-app.get("/project", (req, res) => {
-  res.render("project.ejs");
-});
-app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
-});
 
-app.listen(8080, () => {
-  console.log(`app listening on port 8080`);
-});
+
+app.listen(8080,()=>{
+    console.log(`app listening on port 8080`)
+})
